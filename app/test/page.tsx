8975949f8de2c6a6a6a3921771cd02b9c9155217
Samuel Maxwell <Avornyoh Samuel Obeng>
2026-03-06 10:30:23 +0000

    Simplify games page drastically and add test page

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '50px', color: 'white', background: 'black' }}>
+      <h1 style={{ color: '#00e5ff', fontSize: '32px' }}>TEST PAGE - COMMIT bb49584</h1>
+      <p>If you see this, deployments are working!</p>
+    </div>
+  );
+}
